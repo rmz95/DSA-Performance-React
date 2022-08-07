9b8bc96cd68d9ccc4eb904fe3cdfe15513ad380d
Luis Ramirez <luisramirez.tapia@hotmail.com>
2022-08-07 18:53:42 -0300

    CartFinish Arreglado

--- src/components/cart/CartFinish.jsx
+++ src/components/cart/CartFinish.jsx
@@ -1,62 +1,96 @@
-import React, { useContext, useState } from "react";
-import { db } from "../../firebase/firebase";
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
-import { cartContext } from "../context/CartContext";
+import { useState, useContext } from "react";
+import { cartContext } from '../context/CartContext';
+import { db } from '../../firebase/firebase';
+import {addDoc, collection, serverTimestamp, getDocs, query, orderBy, limit} from 'firebase/firestore';
+import { Link } from "react-router-dom";
 
-    const CartFinish = () => {
-        const { products, qtyProducts } = useContext(cartContext);
 
-        const [idVenta, setidVenta] = useState({
-                nombre:'',
-                telefono:'',
-                email:'',
-                notas:'',
-            })
-        
+const CartFinish = ()=>  {
+    const {products, qtyProducts, clear} = useContext(cartContext);
 
-        const createidVenta = ({target}) => {
-            setidVenta({
-                ...idVenta,
-                [target.name] : target.value
-            })
-        }
+    const formulario = {
+        name:'',
+        email:'',
+        phone:'',
+        creditCard:''
+    }
+    const [orderId, setOrderId] = useState(null);
 
-        const finishBuy = () => {
-            const ventasCollection = collection(db, 'ventas');
-            addDoc(ventasCollection,{
-                idVenta,
+    const [data, setData] = useState(formulario)
+
+    const getData = (e)=> {
+        const {name, value} = e.target;
+        setData({...data, [name]:value})}
+
+    let total = 0;
+        for(let i = 0; i < products.length; i++){
+        total += products[i].price * products[i].qty;
+    } 
+
+    const saveData = async(e)=>{
+        e.preventDefault();
+        try {
+            await addDoc (collection(db,'ventas'),{
+                ...data, 
                 items: products,
                 date: serverTimestamp(),
                 cantidad: qtyProducts,
                 total: total,
             })
-            .then((result) => {
-                setidVenta(result.id)
-            })
-        }
-        
-        let total = 0;
-        for(let i = 0; i < products.length; i++){
-            total += products[i].price * products[i].qty;
-        } 
 
-        return (
-            <>
-            <h3>Ingresa tus datos</h3>
-            <form onSubmit={finishBuy}>
-                <label>Nombre</label>
-                <input onChange={createidVenta} type="text" name="nombre" className="form-control" required></input>
-                <label>Telefono</label>
-                <input onChange={createidVenta} type="number" name="telefono" className="form-control" required></input>
-                <label>Email</label>
-                <input onChange={createidVenta} type="email" name="email" className="form-control"></input>
-                <label>Informacion Adicional</label>
-                <input onChange={createidVenta} type="message" name="notas" className="form-control"></input>
-                <submit onClick={finishBuy} className="btn btn-success" type="submit">
-                    Enviar datos de compra
-                </submit>
-            </form>
-            </>
-        );
+            const q = query(collection(db, "ventas"), orderBy("date", "desc"), limit(1));
+
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+            setOrderId(doc.id);
+            });
+
+            
+            clear();
+
+        } catch (error) {
+            console.log(error);
         }
+
+        setData({...formulario})
+    }
+    
+
+if (orderId){
+    return(
+        <>
+        <div className="py-5 text-center mt-5">
+            <h2 className="mt-5">¡Gracias por elegirnos!</h2>
+            <h4 className="my-5">La compra se ha realizado exitosamente.</h4>
+            <strong>El numero de tu orden es {orderId}</strong> <br/>
+            <Link className="btn btn-outline-primary m-3" to={`/`}>
+            <p className="text-red-600">Volver al Inicio</p>
+            </Link>
+        </div>
+    </>)}
+
+    return (
+        <>
+        <h1>Formulario de Pago</h1>
+        <form onSubmit={saveData}>
+            <input name="name" className="form-control"
+            placeholder="Ingrese su Nombre"
+            type="text" onChange={getData} value={data.formName} required/>
+            <input name= "email" className="form-control"
+            placeholder="Ingrese su Email"
+            type="email" onChange={getData} value={data.formEmail} required/>
+            <input name="phone" className="form-control"
+            placeholder="Telefono de contacto"
+            type="number" onChange={getData} value={data.formPhone} required/>
+            <input name="creditCard" className="form-control"
+            placeholder="Ingrese el numero de Tarjeta"
+            type="number" onChange={getData} value={data.formCard} required/>
+            <button className='form-submit'>Pagar Ahora</button>
+        </form>
+    </>
+    )
+
+}
+
+
 export default CartFinish
